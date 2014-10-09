f0b26f8872c5a9a1553fbda5b473ef12e229f07e
randy <randz_argonillo@yahoo.com>
2014-10-09 18:47:52 +0800

    Added full_title function

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.12'
 group :development, :test  do
   gem 'sqlite3'
   gem 'rspec-rails', '2.9.0'
+  #gem 'guard-rspec'
 end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(title)
+    if title.empty?
+      "Sample App"
+    else
+      "Sample App #{title}"
+    end
+  end
+  
 end
