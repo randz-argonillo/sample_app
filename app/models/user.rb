515a1fb23c53a801a15f1ab1c551e3afd94558a9
randy <randz_argonillo@yahoo.com>
2014-10-19 22:24:21 +0800

    Add validation to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
   validates :name, presence:true, length: {maximum: 50}
+  
+  validates :password, presence:true, length: {minimum: 6}
+  validates :password_confirmation, presence:true
   
   valid_format = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true , format: {with:valid_format}, uniqueness: {case_sensitive:false}
   
   before_save {|user| user.email.downcase!}
+  has_secure_password
 end
--- spec/requests/models/models_user_specs_spec.rb
+++ spec/requests/models/models_user_specs_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe "User model" do
   
-  before{@user = User.new(name:"user1", email:"user1@email.com")}
+  before{@user = User.new(name:"user1", email:"user1@email.com", password:"pwd123", password_confirmation:"pwd123")}
   subject {@user}
   
   it {should respond_to(:name)}
   it {should respond_to(:email)}
+  it {should respond_to(:authenticate)}
   it {should be_valid}
   
   describe "User too long" do
@@ -32,8 +33,8 @@ describe "User model" do
     it "should be valid" do
       valid_emails = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn asdf+@yahoo.com]
       valid_emails.each do |valid_email|
-        email = valid_email
-        should be_valid
+        @user.email = valid_email
+        @user.should be_valid
       end
     end
   end
@@ -49,8 +50,33 @@ describe "User model" do
       should_not be_valid
     end
   end
+  
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " } 
+    it { should_not be_valid }
+  end
 
+  describe "when password doesn't match confirmation" do
+    before {@user.password_confirmation = "foo"}
+    it {should_not be_valid}
+  end
 
+  describe "when password confirmation is nil" do
+    before {@user.password_confirmation = nil}
+    it{should_not be_valid}
+  end
+  
+  #describe "when password is less than 6 characters" do
+  #  before {@user.password = "a" * 5}
+  #  it{should be_invalid}
+  #end
+  
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 } 
+    it { @user.should be_invalid }
+  end
+
+#@user.password_confirmation = 
 end
 
 
