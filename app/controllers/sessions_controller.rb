68b7ddb5c4e65d86816467217cefe47ee95294a3
randy <randz_argonillo@yahoo.com>
2014-10-27 03:21:23 +0800

    making better

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       flash[:success] = "Welcome back #{user.name}"
+      sign_in user
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password'
@@ -16,7 +17,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    
+    sign_out
+    redirect_to root_path
   end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,13 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:session_token])
   end
   
-  def sign_in?
+  def signed_in?
     !current_user.nil?
+  end
+  
+  def sign_out
+    cookes.delete(:session_token)
+    current_user = nil
+    
   end
 end
